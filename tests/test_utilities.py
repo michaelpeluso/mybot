8f0ba1f0f3729c7afdfc28ea9b3a39ee216d5288
Michael Peluso <mfpel29@gmail.com>
2024-02-10 20:37:27 -0500

    update gitignore

--- tests/test_utilities.py
+++
@@ -1,8 +0,0 @@
-from bot.utilities import count_characters
-from faker import Faker
-
-def test_count_characters():
-    count = 27
-    faker = Faker()
-    text = faker.texts(nb_texts = count)
-    assert count_characters(text) == count
